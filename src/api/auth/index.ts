901142fc64b356446fcf97b82dcb2d6aaa8422c6
Leona-cloud <chisomokafor038@gmail.com>
2024-07-07 00:57:10 +0100

    chore: quick fix

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -4,12 +4,13 @@ import { AuthService } from './services';
 import { JwtModule } from '@nestjs/jwt';
 import { expiresIn, jwtSecret } from '@/config';
 import { UserService } from '../user/services';
+import { UserModule } from '../user';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    UserService,
+    UserModule,
     JwtModule.register({
       global: true,
       secret: jwtSecret,
